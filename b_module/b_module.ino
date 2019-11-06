ca75fd3ab298415361d81e3d115fc50ad62900a1
jkh5178 <jkh5178@naver.com>
2019-11-06 10:05:05 +0900

    jkh
    1.b모듈 서버에서 opentime 수신
    
    2. end 명령이 발생기 중지

--- b_module/b_module.ino
+++ b_module/b_module.ino
@@ -10,6 +10,9 @@ const char* password =  "smart1234";
  
 const uint16_t port = 8090;
 const char * host = "192.168.0.2";
+
+int opentime=0
+
 void connectWiFi(const char* ssid,const char* password){
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
@@ -30,37 +33,46 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
     WiFiClient client;
- 
     if (!client.connect(host, port)) {
         Serial.println("Connection to host failed");
         delay(1000);
         return;
     }
+    //B라는 공정 명 서버로 전송
     Serial.println("Connected to server successful!");
     client.print("B");
     Serial.println("B");
-   
+    
     while(client.connected()){
+      //서버에서 start메시지 기다리기
       while(!play_key){
       String m = client.readStringUntil('\n');
       if(m=="start"){
         play_key = true;
         break;
         }
-      }
-      String m=client.readStringUntil('\n');
-      if(m=="stop"){
-        myservo.write(15);
-        delay(2000);
-        myservo.write(0);
-        delay(500);
-        client.print("go");
+       else{
+        opentime=m.toInt();
+        Serial.print("opentime : ")
+        Serial.print(opentime)
         }
+      }
+      //서버에서 메시지 기다리기
+      String m=client.readStringUntil('\n');
+      
+      if(m=="stop"){//stop 메시지가 날라올 경우
+        myservo.write(15);//15도로 서보모터를 열기
+        delay(opentime);//2초간의 딜레이
+        myservo.write(0);//0도로 닫기
+        delay(500);//0.5초간의 딜레이
+        client.print("go");//go메시지 전달
+        }
+        
+      if(m=="end"){//end메시지가 전송될 경우
+        play_key = false;
+       }
       delay(1000);   
       }
-    //Serial.println("Disconnecting...");
-    //client.stop();
     delay(10000);
 }  
