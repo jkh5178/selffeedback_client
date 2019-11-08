59fec8d6661b09cdd99cc41621d7360711a23e89
pang0777 <53871372+pang0777@users.noreply.github.com>
2019-11-08 17:44:58 +0900

    pang0777
    A_module , B_module 주석처리

--- A_module/A_module.ino
+++ A_module/A_module.ino
@@ -1,20 +1,27 @@
 #include <ESP8266WiFi.h>
 #include <Servo.h>
-
-Servo open_servo;
-
-int ray_Value=0;
-int ray_Pin = A0;
-
-bool play_key = false;
+/*
+  A_module
+  공정 기능 : 서보모터를 제어하여 컵디스펜서에서 컵을 떨어뜨린다.
+*/
 
 
-const char* ssid = "smartFactory";
-const char* password =  "smart1234";
+Servo open_servo; //서보모터 제어의 개체
+
+int ray_Value=0; //적외선 센서의 값을 받는 변수
+int ray_Pin = A0; // 적외선 센서의 핀번호
+
+
+bool play_key = false; //공정 작동을 제어하기 위한 bool 변수
+
+
+const char* ssid = "smartFactory"; // 사용할 wifi ID
+const char* password =  "smart1234"; // 사용할 wifi PW
  
-const uint16_t port = 8090;
-const char * host = "192.168.0.2";
+const uint16_t port = 8090; //서버 접속 포트번호
+const char * host = "192.168.0.2"; // 서버의 IP주소
 
+//wemos 보드 wifi 접속 함수
 void connectWiFi(const char* ssid,const char* password){
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
@@ -28,24 +35,25 @@ void connectWiFi(const char* ssid,const char* password){
 
 void setup()
 {
-  Serial.begin(115200);
-  connectWiFi(ssid,password);
+  Serial.begin(115200); // Serial 모니터 값 115200 설정
+  connectWiFi(ssid,password); //wemos 보드 wifi 접속 
   pinMode(ray_Pin, INPUT);
-  open_servo.attach(D8);
-  open_servo.write(0);
+  open_servo.attach(D8); //서보 모터 연결 pin번호
+  open_servo.write(0); // 서보 모터의 값 0으로 초기화
 }
  
 void loop()
 {
     WiFiClient client;
- 
+    //서버 연결 안될 때
+    //1초마다 '연결안됨' 메세지 전송
     if (!client.connect(host, port)) {
         Serial.println("Connection to host failed");
         delay(1000);
         return;
     }
 
-    //서버 연결시 공정 이름 전송
+    //서버 연결시 공정 이름(A) 전송 
     Serial.println("Connected to server successful!");
     client.print("A");
     Serial.println("A");
@@ -53,6 +61,7 @@ void loop()
     while(client.connected()){
       
       while(!play_key && client.connected()){
+        //서버로 부터의 start 메세지 수신 대기
         String m = client.readStringUntil('\n');
         if(m=="start"){
           play_key = true;
@@ -62,6 +71,8 @@ void loop()
       
       //ray_Value = analogRead(ray_Pin);
       //Serial.println(ray_Value);
+      
+      //적외선 값이 100보다 클때(즉 물체인식을 하지 못할 때) 서보모터 제어
       if(analogRead(ray_Pin)>100){
         //start servo
         open_servo.write(120);
--- b_module/b_module.ino
+++ b_module/b_module.ino
@@ -1,20 +1,23 @@
 #include <ESP8266WiFi.h>
 #include <Servo.h>
+/*
+  B_module
+  공정 기능 : 
+*/
+Servo myservo; // 서보모터 제어의 개체
 
-Servo myservo; 
-int pos = 0;
-bool play_key = false;
-bool getvaluecheck=false;
+bool play_key = false; //공정 작동을 제어하기 위한 bool 변수
+bool getvaluecheck=false; // 서버로부터 맞는 값이 들어왔는지 확인하기 위한 bool 변수
 
-const char* ssid = "smartFactory";
-const char* password =  "smart1234";
+const char* ssid = "smartFactory"; // 사용할 wifi ID
+const char* password =  "smart1234"; // 사용할 wifi PW
  
-const uint16_t port = 8090;
-const char * host = "192.168.0.2";
+const uint16_t port = 8090; // 서버 접속 포트번호
+const char * host = "192.168.0.2"; // 서버의 IP주소
 
+int opentime=0; // 서보모터 제어시간 0으로 초기화
 
-int opentime=0;
-
+//wemos 보드 wifi 접속 함수
 void connectWiFi(const char* ssid,const char* password){
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
@@ -27,42 +30,44 @@ void connectWiFi(const char* ssid,const char* password){
   }
 
 void setup() {
-  // put your setup code here, to run once:
-  myservo.attach(D8);
-  Serial.begin(115200);
-  connectWiFi(ssid,password);
-  myservo.write(0);
+  myservo.attach(D8); //서보모터 연결 pin번호
+  Serial.begin(115200); // Serial 모니터 값 115200 설정
+  connectWiFi(ssid,password); // wemos 보드 wifi 접속
+  myservo.write(0); // 초기 서보모터 값 0으로 설정
 }
 
 void loop() {
     WiFiClient client;
-    myservo.write(0);
+    myservo.write(0); 
+    //서버 연결 안될 때
+    //1초마다 '연결안됨' 메세지 전송
     if (!client.connect(host, port)) {
         Serial.println("Connection to host failed");
         delay(1000);
         return;
     }
-    //B라는 공정 명 서버로 전송
+    //서버 연결시 공정 이름(B) 전송
     Serial.println("Connected to server successful!");
-    client.print("C");
-    Serial.println("C");
+    client.print("B");
+    Serial.println("B");
     
     while(client.connected()){
-      //서버에서 start메시지 기다리기
+      //서버로 부터의 start 메세지 수신 대기
       while(!play_key && client.connected()){
       String m = client.readStringUntil('\n');
       if(m=="start"){
         play_key = true;
         break;
         }
+        
        else if(!getvaluecheck){
-        opentime=m.toInt();
+        opentime=m.toInt(); // 서버로부터의 모터 제어시간 int 형으로 받아오기
         Serial.print("opentime : ");
-        Serial.println(opentime);
-        getvaluecheck=true;
+        Serial.println(opentime); // 알맞는 값이 들어왔는지 확인하기 위한 출력
+        getvaluecheck=true; //값을 받아왔을 때 bool 변수 true로 변경
         }
       }
-      //서버에서 메시지 기다리기
+      //서버로부터 메세지 수신대기
       String m=client.readStringUntil('\n');
       if(m=="stop"){//stop 메시지가 날라올 경우
         myservo.write(15);//15도로 서보모터를 열기
@@ -73,9 +78,9 @@ void loop() {
         }
         
       if(m=="end"){//end메시지가 전송될 경우
-        play_key = false;
+        play_key = false; //공정 제어 변수 false로 초기화
        }
-      delay(1000);   
+      delay(1000);   // 1초간 딜레이
       }
     delay(10000);
 }  
